42cb77211ab0e9f1d6f2b271fa429f7096f274a8
Rana Mohammad <ranamohammad@Ranas-MacBook-Pro.local>
2018-05-12 16:14:46 -0500

    created a var lovesCode

--- assessment.js
+++ assessment.js
@@ -9,7 +9,7 @@ var faveNum = 7;
 
 // #3 create a variable called 'lovesCode' and set it to true
 
-// CODE HERE
+var lovesCode = true;
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
